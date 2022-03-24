bb4c39dcfb4bbf93e348250be466564c5b06e240
bszlacht <73062284+bszlacht@users.noreply.github.com>
2022-03-24 21:16:46 +0100

    Add files via upload

---
+++ src/test/java/com/example/NBASTARS/NbastarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NBASTARS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbastarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
